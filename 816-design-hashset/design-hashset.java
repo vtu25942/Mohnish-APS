f5ec3bf0baf4984542a82e1eb8b5ad6ffcc57507
vtu25942 <vtu25942@veltech.edu.in>
2026-04-01 09:24:03 +0530

    Time: 15 ms (42.54%) | Memory: 55.6 MB (18.20%) - LeetSync

---
+++ 816-design-hashset/design-hashset.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class MyHashSet {
+
+    private final int SIZE = 1000;
+    private LinkedList<Integer>[] buckets;
+
+    public MyHashSet() {
+        buckets = new LinkedList[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    private int hash(int key) {
+        return key % SIZE;
+    }
+
+    public void add(int key) {
+        int index = hash(key);
+        if (!buckets[index].contains(key)) {
+            buckets[index].add(key);
+        }
+    }
+
+    public void remove(int key) {
+        int index = hash(key);
+        buckets[index].remove((Integer) key);
+    }
+
+    public boolean contains(int key) {
+        int index = hash(key);
+        return buckets[index].contains(key);
+    }
+}
