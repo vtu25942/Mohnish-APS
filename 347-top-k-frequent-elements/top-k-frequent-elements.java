28562b130c0041faee4edb0e1d1e17548123daf0
vtu25942 <vtu25942@veltech.edu.in>
2026-04-17 09:33:30 +0530

    Time: 11 ms (96.18%) | Memory: 47.8 MB (35.88%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        List<Integer>[] bucket = new List[nums.length + 1];
+        for ( int key : map.keySet()) {
+            int freq = map.get(key);
+            if (bucket[freq] == null) {
+                bucket[freq] = new ArrayList<>();
+            }
+            bucket[freq].add(key);
+        }
+        
+        int[] result = new int[k];
+        int index = 0;
+        
+        for (int i = bucket.length - 1; i >=0 && index < k; i--) {
+            if (bucket[i] != null) {
+                for (int num : bucket[i]) {
+                    result[index++] = num;
+                    if (index ==k) break;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
